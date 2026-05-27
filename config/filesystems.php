7a842a31e6c8db0e5f2aad550eb672a3b940058a
juniyasyos <juniyasyos@gmail.com>
2026-05-27 22:12:37 +0700

    fix: update default filesystem disk configuration to use 's3' in production

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('APP_ENV', 'production') === 'production' ? 's3' : 'local',
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
